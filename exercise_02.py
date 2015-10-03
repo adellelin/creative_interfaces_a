d94da8b152a5b8d8b32ba68de649fa358d002db6
Adelle <Adelle@yingxis-MacBook-Air.local>
2015-10-03 13:32:39 -0400

    completed exercises

--- exercise_02.py
+++ exercise_02.py
@@ -1,4 +1,13 @@
-age  = raw_input("what's your age?")
+#age  = raw_input("what's your age? ")
 
-print "So you're %r old." %(age)
+prompt = ('> ')
+
+print "What's your age? Type in [years],[months] "
+years,months = map(int, raw_input(prompt).split(','))
+
+print "So you're %d years and %d months old." %(years, months)
+
+ageInDays = years * 30 * 12 + months * 30
+
+print "Which is", ageInDays ,"days old." 
 
